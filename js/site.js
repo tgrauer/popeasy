6534b3c576d1e0ac331927ba591c93310b0449a8
Thomas Grauer <tgrauer1@me.com>
2013-05-28 18:10:51 -0400

    init

---
+++ js/site.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.modalLink').modal({
+        trigger: '.modalLink',          // id or class of link or button to trigger modal
+        olay:'div.overlay',             // id or class of overlay
+        modals:'div.modal',             // id or class of modal
+        animationEffect: 'slideDown',   // overlay effect | slideDown or fadeIn | default=fadeIn
+        animationSpeed: 400,            // speed of overlay in milliseconds | default=400
+        moveModalSpeed: 'slow',         // speed of modal movement when window is resized | slow or fast | default=false
+        background: 'a2d3cd',           // hexidecimal color code - DONT USE #
+        opacity: 0.7,                   // opacity of modal |  0 - 1 | default = 0.8
+        openOnLoad: false,              // open modal on page load | true or false | default=false
+        docClose: true,                 // click document to close | true or false | default=true    
+        closeByEscape: true,            // close modal by escape key | true or false | default=true
+        moveOnScroll: true,             // move modal when window is scrolled | true or false | default=false
+        resizeWindow: true,             // move modal when window is resized | true or false | default=false
+        video: 'http://player.vimeo.com/video/2355334?color=eb5a3d',    // enter the url of the video
+        videoClass:'video',             // class of video element(s)
+        close:'.closeBtn'               // id or class of close button
+    });
+});
